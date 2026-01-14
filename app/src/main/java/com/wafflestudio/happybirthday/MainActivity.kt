5e42306c6103aac98071dc7c3fa4cd41f327609e
Yeonu-Kim <ywk0524@snu.ac.kr>
2026-01-14 21:42:50 +0900

    사분할 화면 만들기

--- app/src/main/java/com/wafflestudio/happybirthday/MainActivity.kt
+++ app/src/main/java/com/wafflestudio/happybirthday/MainActivity.kt
@@ -5,9 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -15,12 +20,33 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.wafflestudio.happybirthday.ui.theme.HappyBirthdayTheme
+
+val titleList = listOf(
+    "Text composable",
+    "Image composable",
+    "Row composable",
+    "Column composable"
+)
+val contentList = listOf(
+    "Displays text and follows the recommended Material Design guidelines.",
+    "Creates a composable that lays out and draws a given Painter class object.",
+    "A layout composable that places its children in a horizontal sequence.",
+    "A layout composable that places its children in a vertical sequence."
+)
+val colorList = listOf(
+    Color(0xFFEADDFF),
+    Color(0xFFD0BCFF),
+    Color(0xFFB69DF8),
+    Color(0xFFF6EDFF)
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,7 +58,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    CheckContainer()
+                    FourSectionContainer(
+                        titleList = titleList,
+                        contentList = contentList,
+                        colorList = colorList
+                    )
                 }
             }
         }
@@ -40,26 +70,74 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CheckContainer (modifier: Modifier = Modifier) {
-    val checkImg = painterResource(R.drawable.ic_task_completed)
+fun ContentContainer(title: String, content: String, modifier: Modifier = Modifier) {
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier
+        modifier=modifier
+            .padding(16.dp)
     ) {
-        Image(
-            painter=checkImg,
-            contentDescription = "Task Complete"
-        )
         Text(
-            text="All tasks completed",
+            text = title,
             fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(top=24.dp, bottom = 8.dp)
+            modifier = Modifier.padding(bottom = 16.dp)
         )
         Text(
-            text = "Nice work!",
-            fontSize = 16.sp
+            text = content,
+            textAlign = TextAlign.Justify,
         )
+    }
+}
+@Composable
+fun FourSectionContainer(
+    titleList: List<String>,
+    contentList: List<String>,
+    colorList: List<Color>,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier=modifier.fillMaxSize()) {
+        Row(modifier = modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+        ) {
+            ContentContainer(
+                title = titleList[0],
+                content = contentList[0],
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(colorList[0])
+            )
+            ContentContainer(
+                title = titleList[1],
+                content = contentList[1],
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(colorList[1])
+            )
+        }
+        Row(modifier = modifier
+            .fillMaxWidth()
+            .weight(1f)
+        ) {
+            ContentContainer(
+                title = titleList[2],
+                content = contentList[2],
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(colorList[2])
+            )
+            ContentContainer(
+                title = titleList[3],
+                content = contentList[3],
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(colorList[3])
+            )
+        }
     }
 }
 
@@ -67,6 +145,10 @@ fun CheckContainer (modifier: Modifier = Modifier) {
 @Composable
 fun CheckPreview() {
     HappyBirthdayTheme {
-        CheckContainer()
+        FourSectionContainer(
+            titleList = titleList,
+            contentList = contentList,
+            colorList = colorList
+        )
     }
 }

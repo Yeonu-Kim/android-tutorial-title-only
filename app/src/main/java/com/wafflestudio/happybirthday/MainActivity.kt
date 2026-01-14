587d7afdc3145f51337dc89b4414ec7f620bf19f
Yeonu-Kim <ywk0524@snu.ac.kr>
2026-01-14 20:42:55 +0900

    뉴스 화면 만들기

--- app/src/main/java/com/wafflestudio/happybirthday/MainActivity.kt
+++ app/src/main/java/com/wafflestudio/happybirthday/MainActivity.kt
@@ -1,25 +1,21 @@
 package com.wafflestudio.happybirthday
 
-import android.R.attr.name
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -32,13 +28,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val title = getString(R.string.title)  // 단일 문자열
+        val contentList = resources.getStringArray(R.array.content_list).toList()  // 문자열 배열
         setContent {
             HappyBirthdayTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingCard(stringResource(R.string.card_text), from= stringResource(R.string.card_sender))
+                    News(title=title, content=contentList)
                 }
             }
         }
@@ -46,59 +44,75 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingCard(message: String, from: String, modifier: Modifier = Modifier) {
-    val image = painterResource(R.drawable.androidparty)
-    Box(modifier=modifier) {
+fun News(title: String, content: List<String>, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.bg_compose_background)
+    Column(modifier=modifier) {
         Image(
             painter = image,
             contentDescription = null,
-            contentScale = ContentScale.Crop,
-            alpha = 0.5F
+            modifier = Modifier.fillMaxWidth()
         )
-        GreetingText(
-            message=message,
-            from=from,
-            modifier=Modifier
-                .fillMaxSize()
-                .padding(4.dp)
+        NewsContent(
+            title=title,
+            content=content,
         )
     }
 }
 
 @Composable
-fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+fun NewsContent(title: String, content: List<String>, modifier: Modifier = Modifier) {
     Column(
         verticalArrangement = Arrangement.Center,
-        modifier=modifier.padding(8.dp)
+        modifier=modifier
+            .fillMaxWidth()
     ) {
         Text(
-            text = message,
-            fontSize = 80.sp,
-            lineHeight = 100.sp,
-            textAlign = TextAlign.Center
-        )
-        Text(
-            text = "from $from",
-            fontSize = 36.sp,
+            text = title,
+            fontSize = 24.sp,
+            textAlign = TextAlign.Start,
             modifier = Modifier
-                .padding(16.dp)
-                .align(alignment = Alignment.End)
+                .padding(
+                    start = 16.dp,
+                    end = 16.dp,
+                    top = 16.dp,
+                    bottom = 8.dp
+                )
         )
+        content.forEach {
+            item -> Text(
+                text = item,
+                fontSize = 16.sp,
+                textAlign = TextAlign.Justify,
+                modifier = Modifier
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+                    .fillMaxWidth()
+            )
+        }
+
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun BirthdayCardPreview() {
+fun NewsContentPreview() {
+    val title = stringResource(R.string.title)
+    val context = LocalContext.current
+    val contentList = context.resources.getStringArray(R.array.content_list).toList()
+
     HappyBirthdayTheme {
-        GreetingText("Happy Birthday Sam!", from="Emma")
+        NewsContent(title, contentList)
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun BirthdayImagePreview() {
+fun NewsPreview() {
+    val title = stringResource(R.string.title)
+    val context = LocalContext.current
+    // TODO: Warning 없이 사용하는 방법?
+    val contentList = context.resources.getStringArray(R.array.content_list).toList()
+
     HappyBirthdayTheme {
-        GreetingCard("Happy Birthday Sam!", from="Emma", )
+        News(title, contentList)
     }
 }

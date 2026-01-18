25c5e757a65bb148972e4fe2f6c78084eee1f9a8
Yeonu-Kim <ywk0524@snu.ac.kr>
2026-01-18 22:18:50 +0900

    버튼 클릭 시 주사위가 굴러가는 앱 개발

--- app/src/main/java/com/wafflestudio/happybirthday/MainActivity.kt
+++ app/src/main/java/com/wafflestudio/happybirthday/MainActivity.kt
@@ -10,14 +10,22 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -27,26 +35,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.wafflestudio.happybirthday.R.*
 import com.wafflestudio.happybirthday.ui.theme.HappyBirthdayTheme
-
-val titleList = listOf(
-    "Text composable",
-    "Image composable",
-    "Row composable",
-    "Column composable"
-)
-val contentList = listOf(
-    "Displays text and follows the recommended Material Design guidelines.",
-    "Creates a composable that lays out and draws a given Painter class object.",
-    "A layout composable that places its children in a horizontal sequence.",
-    "A layout composable that places its children in a vertical sequence."
-)
-val colorList = listOf(
-    Color(0xFFEADDFF),
-    Color(0xFFD0BCFF),
-    Color(0xFFB69DF8),
-    Color(0xFFF6EDFF)
-)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,97 +48,52 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    FourSectionContainer(
-                        titleList = titleList,
-                        contentList = contentList,
-                        colorList = colorList
-                    )
+                    DiceRollerApp()
                 }
             }
         }
     }
 }
 
-@Composable
-fun ContentContainer(title: String, content: String, modifier: Modifier = Modifier) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier=modifier
-            .padding(16.dp)
-    ) {
-        Text(
-            text = title,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-        Text(
-            text = content,
-            textAlign = TextAlign.Justify,
-        )
+fun randomDice():Int {
+    return (1..6).random()
+}
+
+fun getDiceImage(diceNumber: Int): Int {
+    return when (diceNumber) {
+        1 -> drawable.dice_1
+        2 -> drawable.dice_2
+        3 -> drawable.dice_3
+        4 -> drawable.dice_4
+        5 -> drawable.dice_5
+        else -> drawable.dice_6
     }
 }
 @Composable
-fun FourSectionContainer(
-    titleList: List<String>,
-    contentList: List<String>,
-    colorList: List<Color>,
-    modifier: Modifier = Modifier
-) {
-    Column(modifier=modifier.fillMaxSize()) {
-        Row(modifier = modifier
-                    .fillMaxWidth()
-                    .weight(1f)
-        ) {
-            ContentContainer(
-                title = titleList[0],
-                content = contentList[0],
-                modifier = Modifier
-                    .weight(1f)
-                    .fillMaxHeight()
-                    .background(colorList[0])
-            )
-            ContentContainer(
-                title = titleList[1],
-                content = contentList[1],
-                modifier = Modifier
-                    .weight(1f)
-                    .fillMaxHeight()
-                    .background(colorList[1])
-            )
-        }
-        Row(modifier = modifier
-            .fillMaxWidth()
-            .weight(1f)
-        ) {
-            ContentContainer(
-                title = titleList[2],
-                content = contentList[2],
-                modifier = Modifier
-                    .weight(1f)
-                    .fillMaxHeight()
-                    .background(colorList[2])
-            )
-            ContentContainer(
-                title = titleList[3],
-                content = contentList[3],
-                modifier = Modifier
-                    .weight(1f)
-                    .fillMaxHeight()
-                    .background(colorList[3])
-            )
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    var diceNumber by remember { mutableStateOf(1) }
+    fun onClickButton() {
+        diceNumber= randomDice()
+    }
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(getDiceImage(diceNumber)),
+            contentDescription = "1"
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick={onClickButton()}) {
+            Text("Roll")
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun CheckPreview() {
-    HappyBirthdayTheme {
-        FourSectionContainer(
-            titleList = titleList,
-            contentList = contentList,
-            colorList = colorList
-        )
-    }
+fun DiceRollerApp() {
+    DiceWithButtonAndImage()
 }
+
